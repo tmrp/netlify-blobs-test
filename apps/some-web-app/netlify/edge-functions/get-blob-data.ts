3581dddfa77211c81adcbea1d5d3db0db0976a86
Timothy Peterson <33550033+tmrp@users.noreply.github.com>
2024-03-12 09:54:40 +0100

    added netlify blob store

--- apps/some-web-app/netlify/edge-functions/get-blob-data.ts
+++ apps/some-web-app/netlify/edge-functions/get-blob-data.ts
@@ -1,3 +1,11 @@
 import { Context } from '@netlify/edge-functions';
+import { someBlobStore } from './helpers/store/blob-store.ts';
 
-export default async (request: Request, context: Context) => {};
+export default async (request: Request, context: Context) => {
+  return new Response(await someBlobStore.get('some-blob-key'), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
